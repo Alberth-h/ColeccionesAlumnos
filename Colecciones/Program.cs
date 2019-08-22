0a81f52d3250e20977f1f66702d170e32a8c2dba
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 12:22:44 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+
+            alumnos.Add(new Alumno("Jose", "12345"));
+            alumnos.Add(new Alumno("Ana", "67890"));
+
+            foreach (Alumno alumno in alumnos)
+            {
+                Console.WriteLine("Nombre: " + alumno.Nombre);
+            }
+
+            Console.Read();
+        }
+    }
+}
